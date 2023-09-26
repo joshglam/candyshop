3212a5d2fcbe2031189fdf8cd1e8220a751cca5a
melissamedina <melissa.med52@gmail.com>
2023-09-26 14:33:37 -0400

    CandyContainer-update

--- src/CandyContainer.jsx
+++ src/CandyContainer.jsx
@@ -1,15 +1,42 @@
+import React from'react';
+import Search from './Search';
 import CandyCard from "./CandyCard"
-import Search from "./Search"
+import {useState, useEffect} from "react";
 
-export default function CandyContainer() {
+function CandyContainer() {
+    const [candy, setCandy]= useState([])
+    const [ search, setSearch] = useState('')
+
+    useEffect(()=> { 
+        fetch(" http://127.0.0.1:5174/")
+        .then(resp => resp.json())
+        .then(candies => setCandy(candies))
+
+    },[])
+
+    const filteredCandy = candy.filter(candy => {
+        return 
+            candy.name.toLowerCase().includes(
+            search.toLowerCase(),
+            candy.desc.toLowerCase().includes(
+                search.toLowerCase()
+            ))
+        
+    })
+
+
+
+
     return (
         <div>
-            <p> header for candy container </p>
-            <Search />
-            <div>
-               {/* here you'll .map the candy cards */}
-                <CandyCard />
+            <Search setSearch={setSearch} />
+            <div className="cards">
+                {candy.map(candy => {
+                    return <CandyCard key={candy.id} {...candy} />
+                })}
             </div>
         </div>
     )
-}
+            }
+
+export default CandyContainer;
